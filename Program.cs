de3091c787d8fb0458627d1a5073ff3b7a32d83c
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2024-09-10 17:44:49 +0300

    10.09.24

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //kategori etiketi bir değer tutucudur.
+
+            string kategoriEtiketi = "Kategori";
+
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirişYapmişMi = false; //Sisteme giriş yapamış mı?
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+
+
+            if (sistemeGirişYapmişMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+        }
+    }
+}
